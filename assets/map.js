a065102232836b7c8a4c35ccc82d6aed2cdca668
EmrahFerecov <emrah.ferecov13@gmail.com>
2023-10-12 08:09:28 +0400

    first commit

---
+++ assets/map.js
@@ -0,0 +1,31 @@
+// let cem = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 || i % 5 === 0) {
+//         cem += i;
+//     }
+// }
+
+// console.log("3-ə və ya 5-ə bölünən ədədlərin cəmi: " + cem);
+// function isPowerOfTwo(number) {
+//     return (number & (number - 1)) === 0 && number !== 0;
+// }
+
+// if (isPowerOfTwo(eded)) {
+//     console.log(eded + " 2-nin quvvətidir.");
+// } else {
+//     console.log(eded + " 2-nin quvvəti deyil.")}
+// function isPrime(number) {
+//     if (number <= 1) return false;
+//     if (number <= 3) return true;
+//     if (number % 2 === 0 || number % 3 === 0) return false;
+//     for (let i = 5; i * i <= number; i += 6) {
+//         if (number % i === 0 || number % (i + 2) === 0) return false;
+//     }
+//     return true;
+// }
+
+// let eded = 17;
+
+// if (isPrime(eded)) console.log(eded + " sadədir.");
+// else console.log(eded + " mürəkkəbdir.");
